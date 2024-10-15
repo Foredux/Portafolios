71d0c3d8272bd923938e1dd15f2804e24edd4c79
Eduardo Rodriguez <114937476+Foredux@users.noreply.github.com>
2024-10-15 23:17:28 +0200

    Cambios en css y fixeando componente Contact

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -3,74 +3,66 @@ import React from "react";
 export const Contacto = () => {
   return (
     <div className="contact-container">
-      <h1>Contacto</h1>
+      <h1 className="contact-title">Contacto</h1>
 
-      <form
-        className="contact-form"
-        action="https://getform.io/f/bolgvppa"
-        method="POST"
-      >
-        <input
-          type="text"
-          id="nombre"
-          name="nombre"
-          required
-          placeholder="Nombre"
-        />
+      <form action="https://getform.io/f/bolgvppa" method="POST">
+        <div className="inputBox">
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            required
+            className="inputField"
+            placeholder=" "
+          />
+          <label className="inputLabel" htmlFor="nombre">
+            Nombre
+          </label>
+        </div>
 
-        <input
-          type="text"
-          id="apellidos"
-          name="apellidos"
-          required
-          placeholder="Apellidos"
-        />
+        <div className="inputBox">
+          <input
+            type="text"
+            id="apellidos"
+            name="apellidos"
+            required
+            className="inputField"
+            placeholder=" "
+          />
+          <label className="inputLabel" htmlFor="apellidos">
+            Apellidos
+          </label>
+        </div>
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-          placeholder="Correo electrónico"
-        />
+        <div className="inputBox">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="inputField"
+            placeholder=" "
+          />
+          <label className="inputLabel" htmlFor="email">
+            Correo Electrónico
+          </label>
+        </div>
 
-        <textarea
-          id="mensaje"
-          name="mensaje"
-          required
-          placeholder="Motivo de Contacto"
-        ></textarea>
+        <div className="inputBox">
+          <textarea
+            id="mensaje"
+            name="mensaje"
+            required
+            className="textAreaField"
+            placeholder=" "
+          ></textarea>
+          <label className="inputLabel" htmlFor="mensaje">
+            Motivo de Contacto
+          </label>
+        </div>
 
-        <button type="submit" className="cta">
-          <span>Enviar</span>
-          <svg
-            width="15px"
-            height="15px"
-            viewBox="0 0 66 43"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g
-              id="arrow"
-              stroke="none"
-              strokeWidth="1"
-              fill="none"
-              fillRule="evenodd"
-            >
-              <path
-                className="one"
-                d="M40.154393,3.20897343 C40.154393,3.20897343 44.4561631,-0.988982101 46.7178461,1.12300894 C48.9795292,3.23500001 45.6150864,6.10640747 45.6150864,6.10640747"
-              ></path>
-              <path
-                className="two"
-                d="M48.6084747,9.77634593 C48.6084747,9.77634593 57.3430457,14.2890166 55.9644182,17.7993766 C54.5857907,21.3097366 49.2120513,18.7969126 49.2120513,18.7969126"
-              ></path>
-              <path
-                className="three"
-                d="M33.5922066,29.5488663 C33.5922066,29.5488663 37.2823973,32.3445693 35.7850174,35.8772987 C34.2876374,39.4100281 29.1822354,36.5727991 29.1822354,36.5727991"
-              ></path>
-            </g>
-          </svg>
+        <button type="submit" className="submitButton">
+          Enviar
         </button>
       </form>
     </div>
